3b87637b8de763ae70ccde0d81e14e7b870ae1ee
EvelynProto <jeremyruss43@gmail.com>
2021-07-06 21:52:54 +0100

    Small QOL update
    added loading thingy :flushed:

---
+++ loader.js
@@ -0,0 +1,22 @@
+// LOADING SCRIPT
+function getOff() {
+	let id = null;
+	const elem = document.getElementById("loading");
+	let pos = 1;
+	clearInterval(id);
+	id = setInterval(frame, 1);
+	function frame() {
+		if (pos <= 0) {
+			clearInterval(id);
+			elem.remove();
+		} else {
+			pos -= 0.1;
+			elem.style.opacity = pos;
+		}
+	}
+}
+
+$(document).ready(function() {
+	// Animate loader off screen
+	setTimeout(function() {getOff();}, 500);
+});
