1180ea7111b444e6441beae9a6299fb9cf75a6ca
BuildTools <unconfigured@null.spigotmc.org>
2021-08-27 14:58:06 +0100

    Added particles moving away from mouse on index page

--- indexsketch.js
+++ indexsketch.js
@@ -1,6 +1,9 @@
-numParticles = 50;
+numParticles = 100;
 width = 0;
 height = 0;
+radius = 100;
+let cnv;
+fps = 0;
 
 function Particle() {
 	this.pos = createVector(random() * width, random() * height);
@@ -20,6 +23,8 @@ function Particle() {
 			this.vel.y *= -1;
 		}
 		this.pos.add(this.vel);
+		this.pos.x = constrain(this.pos.x, 0, width);
+		this.pos.y = constrain(this.pos.y, 0, height);
 	}
 }
 
@@ -27,12 +32,12 @@ function setup() {
 	let main = document.querySelector('main');
 	width = main.offsetWidth;
 	height = main.offsetHeight;
-	createCanvas(width, height);
+	cnv = createCanvas(width, height);
 	particles = [];
 	for (var i = 0; i < numParticles; i++) {
 		particles[i] = new Particle();
 	}
-	frameRate(30);
+	frameRate(60);
 }
 
 function draw() {
@@ -40,17 +45,37 @@ function draw() {
 	for (var x = 0; x < numParticles; x++) {
 		particles[x].update();
 		particles[x].show();
+		// Move particles if too close to mouse
+		let distance = dist(particles[x].pos.x, particles[x].pos.y, mouseX, mouseY);
+		if (distance < radius) {
+			let mouse = createVector(mouseX, mouseY);
+			let difference = p5.Vector.sub(mouse, particles[x].pos);
+			difference.setMag(map(radius / distance, 1, 15, 1, 200));
+			particles[x].pos.sub(difference);
+		}
+
 		for (var y = 0; y < numParticles; y++) {
 			if (y == x) {
 				continue;
 			}
-			var distance = dist(particles[x].pos.x, particles[x].pos.y, particles[y].pos.x, particles[y].pos.y);
-			if (distance > 200) {
+			distance = dist(particles[x].pos.x, particles[x].pos.y, particles[y].pos.x, particles[y].pos.y);
+			if (distance > 250) {
 				continue;
 			}
-			stroke('rgba(255,255,255,' + (1 - (distance / 200)) + ')');
+			stroke('rgba(255,255,255,' + (1 - (distance / 250)) + ')');
 			strokeWeight(1);
 			line(particles[x].pos.x, particles[x].pos.y, particles[y].pos.x, particles[y].pos.y);
 		}
+
+		if (random() < 0.01) {
+			particles[x].vel = p5.Vector.random2D();
+		}
 	}
+	fps++;
+	stroke(255);
+	textSize(16);
+	if (keyIsDown(81)) {
+		text("FPS: " + fps, 10, 150);
+	}
+	setTimeout(function() {fps--;}, 1000);
 }
